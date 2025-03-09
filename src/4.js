fda533abe22e53c820bc0b61d0186c1150d9f20b
callofeder345 <callofeder345@gmail.com>
2025-03-09 07:12:58 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,11 @@
+<script>
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const numbers = [];
+for (let i = 0; i < 10; i++) {
+  numbers.push(getRandomInt(10));
+}
+console.log(numbers);
+</script>
